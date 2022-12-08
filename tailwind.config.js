a3c5ff53ab3922b1e65c444091334766c63af986
mike-koala-bear <55577@pm.me>
2022-12-08 06:49:16 +0000

    Part 1 setting up the app

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("tailwindcss-children"),
+  ],
+  // ...
+};
